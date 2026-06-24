59659a064745319a62fed7d21f51367d7c124336
Bino sunny <binosunny12@gmail.com>
2026-06-24 11:17:23 +0530

    registerForm update

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Data } from '../servies/data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone:true,
-  imports: [FormsModule,RouterLink],
+  imports: [FormsModule,RouterLink,CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -27,6 +28,23 @@ export class Register {
    np:'',
    cp:''
  }
+
+
+   states = [
+    { id: 1, name: 'Kerala' },
+    { id: 2, name: 'Tamil Nadu' },
+    { id: 3, name: 'Karnataka' },
+    { id: 4, name: 'Andhra Pradesh' },
+    { id: 5, name: 'Telangana' }
+  ];
+  
+  countrys=[
+    {id:1,name:"india"},
+    {id:2,name:"United States"},
+    {id:3,name:"china"},
+    {id:3,name:"UAE"}
+  ]
+
   
 constructor(private ds:Data){}
 
